5410fbdf13f648e1811ed212d77efca1de69813e
joao vitor <freitaslima155@gmail.com>
2025-10-04 00:44:31 -0300

    editando depoimentos

--- src/app/components/home/depoimentos/depoimentos.component.ts
+++ src/app/components/home/depoimentos/depoimentos.component.ts
@@ -12,19 +12,19 @@ export class DepoimentosComponent {
   listaDepoimentos = [
     {
       citacao: '"A Alessandra foi um divisor de águas na jornada do nosso filho. Com sua abordagem sensível e técnicas eficazes, vimos uma melhora incrível não só nas notas, mas na confiança e no prazer em aprender. Somos eternamente gratos."',
-      autor: 'Mariana L.',
-      descricao: 'Mãe do Lucas, 8 anos',
+      autor: 'Jucilene Peixoto.',
+      descricao: 'Mãe do joyce, 12 anos',
       imagemUrl: 'assets/imagens/depoimento-1.jpg' 
     },
     {
       citacao: '"Como diretor de uma escola, posso afirmar que a consultoria da Alessandra transformou nossa abordagem de inclusão. Ela nos deu ferramentas práticas e uma nova perspectiva que beneficiaram toda a comunidade escolar."',
-      autor: 'Ricardo S.',
+      autor: 'Ana Paula Procopio.',
       descricao: 'Diretor Escolar',
       imagemUrl: 'assets/imagens/depoimento-2.jpg' 
     },
     {
       citacao: '"Meu filho tinha muita dificuldade de concentração. O trabalho individualizado e o carinho da Alessandra o ajudaram a se encontrar nos estudos e a desenvolver seu potencial. Hoje ele é um aluno muito mais feliz e seguro."',
-      autor: 'Fernanda P.',
+      autor: 'Aline Freitas.',
       descricao: 'Mãe da Sofia, 10 anos',
       imagemUrl: 'assets/imagens/depoimento-3.jpg' 
     }
