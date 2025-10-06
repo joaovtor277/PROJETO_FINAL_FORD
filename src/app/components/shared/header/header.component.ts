8766d6cad54fe35268178779c26767491ec51ef5
joao vitor <freitaslima155@gmail.com>
2025-10-06 15:35:03 -0300

    botoes_hero_habilitado

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router'; // <-- MUITO IMPORTANTE: Usaremos este
+import { RouterModule } from '@angular/router'; 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ RouterModule ], // <-- Garantimos que tudo relacionado a rotas está importado
+  imports: [ RouterModule ], 
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
