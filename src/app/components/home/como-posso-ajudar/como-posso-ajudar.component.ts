c8097b952079f670068e4563f7048d643850e429
joao vitor <freitaslima155@gmail.com>
2025-10-06 16:34:45 -0300

    atualizaçao_como_posso_ajudar

--- src/app/components/home/como-posso-ajudar/como-posso-ajudar.component.ts
+++ src/app/components/home/como-posso-ajudar/como-posso-ajudar.component.ts
@@ -12,19 +12,39 @@ export class ComoPossoAjudarComponent {
   listaDeServicos = [
     {
       icone: 'imagens/icone lupa cerebro.png',
-      titulo: 'Avaliação Psicopedagógica' 
+      titulo: 'Avaliação Psicopedagógica',
+      descricao: 'Uma análise detalhada para entender as forças e desafios do aprendizado, identificando as melhores estratégias para o desenvolvimento educacional.',
+      isOpen: false
     },
     {
       icone: 'imagens/icone individuo.png',
-      titulo: 'Intervenção Individualizada'
+      titulo: 'Intervenção Individualizada',
+      descricao: 'Planos de ação personalizados com atividades e técnicas específicas para superar dificuldades de aprendizagem e potencializar habilidades.',
+      isOpen: false
     },
     {
       icone: 'imagens/icone familia.png',
-      titulo: 'Orientação Parental'
+      titulo: 'Orientação Parental',
+      descricao: 'Apoio e direcionamento para pais e responsáveis, oferecendo ferramentas para criar um ambiente estimulante e positivo para o aprendizado em casa.',
+      isOpen: false
     },
     {
       icone: 'imagens/icone consulta pedagogica.png',
-      titulo: 'Consultoria Pedagógica'
+      titulo: 'Consultoria Pedagógica',
+      descricao: 'Assessoria para escolas e educadores na implementação de práticas inclusivas e no desenvolvimento de currículos adaptados às diversas necessidades dos alunos.',
+      isOpen: false
     }
   ];
+
+  toggleServico(servicoClicado: any) {
+    this.listaDeServicos.forEach(servico => {
+      if (servico !== servicoClicado) {
+        servico.isOpen = false;
+      }
+    });
+
+    servicoClicado.isOpen = !servicoClicado.isOpen;
+  }
 }
+
+
