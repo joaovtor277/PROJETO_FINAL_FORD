f50148311b9291c0b62a2b59df2d78c965d5f9da
joao vitor <freitaslima155@gmail.com>
2025-10-14 22:12:11 -0300

    atualização_carrosel_galeria

--- src/app/components/home/galeria/galeria.component.ts
+++ src/app/components/home/galeria/galeria.component.ts
@@ -3,46 +3,64 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-galeria',
+  standalone: true, 
   imports: [CommonModule],
   templateUrl: './galeria.component.html',
-  styleUrl: './galeria.component.css'
+  styleUrls: ['./galeria.component.css'] 
 })
 export class GaleriaComponent {
-imagensGaleria = [
+  
+  indiceAtual = 0;
+
+  
+  imagensGaleria = [
     {
       caminho: 'imagens/imagem 1 galeria.jpg',
       alt: 'Criança sorrindo durante atividade',
-      classeCss: 'item-1'
     },
     {
       caminho: 'imagens/imagem 2 galeria.jpg',
       alt: 'Sala de aula inclusiva',
-      classeCss: 'item-2'
     },
     {
       caminho: 'imagens/imagem 3 galeria.jpg',
       alt: 'Profissional com família',
-      classeCss: 'item-3'
     },
     {
       caminho: 'imagens/imagem 4 galeria.jpg',
       alt: 'Crianças sorrindo juntas',
-      classeCss: 'item-4'
     },
     {
       caminho: 'imagens/imagem 5 galeria.jpg',
       alt: 'Atividade de artes',
-      classeCss: 'item-5'
     },
     {
       caminho: 'imagens/imagem 6 galeria.jpg',
       alt: 'Criança concentrada',
-      classeCss: 'item-6'
     },
     {
       caminho: 'imagens/imagem 7 galeria.jpg',
       alt: 'Momento de descoberta',
-      classeCss: 'item-7'
     }
   ];
+
+  
+  public slideAnterior(): void {
+    if (this.indiceAtual > 0) {
+      this.indiceAtual--;
+    } else {
+      
+      this.indiceAtual = this.imagensGaleria.length - 1;
+    }
+  }
+
+  
+  public proximoSlide(): void {
+    if (this.indiceAtual < this.imagensGaleria.length - 1) {
+      this.indiceAtual++;
+    } else {
+     
+      this.indiceAtual = 0;
+    }
+  }
 }
---
+++ src/app/features/agendamento/pages/meus-agendamentos/meus-agendamentos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meus-agendamentos',
+  imports: [],
+  templateUrl: './meus-agendamentos.component.html',
+  styleUrl: './meus-agendamentos.component.css'
+})
+export class MeusAgendamentosComponent {
+
+}
